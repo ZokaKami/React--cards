af575c129c0e928550c7f1762afe336d3c09e748
ZokaKami <ruffy92@live.com>
2023-08-24 10:27:11 +0200

    First commit

---
+++ components/cards.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="w-full">
+      <div className=" container  mx-auto w-fit pt-4">
+        <input
+          className="rounded-3xl p-4   border-solid border-2   "
+          placeholder="Enter Post Title"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import Header from "../components/header";
+import Cards from "../components/cards";
+import React from "react";
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Cards/>
+    </>
+  )
+}
+
+export default App
