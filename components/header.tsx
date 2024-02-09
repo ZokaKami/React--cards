ba6cbf04ef956598d85f8ed551d328db20a79fcc
ZokaKami <ruffy92@live.com>
2024-02-09 18:04:09 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -10,16 +10,7 @@ function Header() {
   return (
     <div className="w-full py-6">
       <div className=" container  mx-auto w-fit  ">
-        <form onSubmit={testApp}>
-          <input
-            type="text"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value)}
-            className="rounded-3xl  p-4  border-solid border-2   "
-            placeholder="Enter Post Title"
-          />
-          <button>✓</button>
-        </form>
+       
         <h1> </h1>
       </div>
     </div>
